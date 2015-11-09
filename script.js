9a0bb0176df82e7020bcb4b93599bef7ab97adc6
tal735 <tal735@gmail.com>
2015-11-09 18:47:13 +0200

    Aaron's solution. KILL ME NOW

--- script.js
+++ script.js
@@ -8,12 +8,14 @@ function loadjscssfile(filename, filetype) {
     if (typeof fileref!="undefined")
         document.getElementsByTagName("head")[0].appendChild(fileref);
 }
-loadjscssfile("//f.vimeocdn.com/js/froogaloop2.min.js", "js") //dynamically load and add this .js file
+
+loadjscssfile("//f.vimeocdn.com/js/froogaloop2.min.js", "js"); //dynamically load and add this .js file
+loadjscssfile("rtfl_froogaloop.js", "js"); //dynamically load and add this .js file
 
 // store all vimeo iframes for later reference
 var vimeo_iframes = []
-//detect vimeo video regex
-var regex = /(.)*player.vimeo.com(.)*$/i;
+//detect vimeo video vimeo_src_regex
+var vimeo_src_regex = /(.)*player.vimeo.com(.)*$/i;
 //get all iframes in page
 var iframes=$('iframe');
 //look only for vimeo iframes
@@ -21,21 +23,20 @@ for (i=0; i<iframes.length; i++) {
     /// FIX SRC ATTRIBUTE IF NEEDED:
     // "https://player.vimeo.com/video/76979866" ----> "https://player.vimeo.com/video/76979866?api=1"
     var iframe = iframes[i];
-    var src_attr = iframe.getAttribute('src');
+    var iframe_src_attr = iframe.getAttribute('src');
     //check if 'src' is of form '...player.vimeo.com/...'
-    if (regex.test(src_attr)) {
-
-        //check if 'api=1' exists in 'src'              /**********************************/
-                                                        /*                                */
-        var regex2 = /(.)*api=1(.)*$/i;                 /*     Adding events works        */
-        if (!regex2.test(src_attr)) {                   /*     also without modifying     */
-            //if not, add it                            /*     src to &/?api=1            */
-            if (src_attr.indexOf('?') == -1) {          /*                                */
-                src_attr = src_attr + '?api=1';         /**********************************/
+    if (vimeo_src_regex.test(iframe_src_attr)) {
+        //check if 'api=1' exists in 'src'                      /**********************************/
+                                                                /*                                */
+        var api_regex = /(.)*api=1(.)*$/i;                      /*     Adding events works        */
+        if (!api_regex.test(iframe_src_attr)) {                 /*     also without modifying     */
+            //if not, add it                                    /*     src to &/?api=1            */
+            if (iframe_src_attr.indexOf('?') == -1) {           /*                                */
+                iframe_src_attr = iframe_src_attr + '?api=1';   /**********************************/
             } else {
-                src_attr = src_attr + '&api=1';
+                iframe_src_attr = iframe_src_attr + '&api=1';
             }
-            iframe.setAttribute('src', src_attr);
+            iframe.setAttribute('src', iframe_src_attr);
         }
 
         //save only vimeo iframes
@@ -63,3 +64,10 @@ $( window ).load(function() {
             });
         }
 });
+
+/*
+ NOTES:
+    1. api=1 in src is not necessary for some reason...
+    2. same goes for playerid (id=..) which vimeo states is necessary
+    3.
+ */
